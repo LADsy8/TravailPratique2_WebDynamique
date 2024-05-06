e68ff919e7e76c1096f5352a0a936710c20b786b
LADsy8 <dube.arthur@gmail.com>
2024-05-06 17:41:17 -0400

    0.0.2.3: commencer index-view
    il reste le texte

--- html/index-view.php
+++ html/index-view.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,14 +18,35 @@
                 
                 -->
     <script src="js/app.js" defer></script>
-    <?php require_once __DIR__."/../live-reload.php"; ?>
+    <?php require_once __DIR__ . "/../live-reload.php"; ?>
 </head>
+
 <body class="bg-black">
-    <header>
-        
-    </header>
-    <main>
-        
+
+    <nav class="px-6 py-4 flex text-white h-max content-center justify-between">
+        <div class="justify-self-start">
+            <a class="flex" href="index.php">
+                <img src="img/noto_blank.svg" class="h-6 w-6 lg:h-6 lg:w-6" alt="Noto" />
+                <span class="flex text-white px-2">Noto</span>
+            </a>
+        </div>
+        <div class="justify-items-end">
+            <a class="hover:opacity-80 px-5 py-2" href="#">
+                Créer mon compte
+            </a>
+            <a class="hover:opacity-80 px-5 py-2 border-white border-2 rounded" href="#">
+                Se connecter
+            </a>
+        </div>
+    </nav>
+    <div class="h-12">
+
+    </div>
+    <main class="h-10 p-0 m-o self-center">
+        <div class=" border-white border-2 self-center space-around">
+
+        </div>
     </main>
 </body>
+
 </html>
